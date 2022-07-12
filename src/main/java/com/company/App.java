b02b3e56f4657edd0ed0dd6c57b94f77651c447e
sihwa1019 <epretty@naver.com>
2022-07-12 15:08:21 +0900

    명언수정

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -37,6 +37,10 @@ public class App {
                     remove(rq);
                     break;
 
+                case "수정":
+                    modify(rq);
+                    break;
+
                 case "목록":
                     list(rq);
                     break;
@@ -47,6 +51,36 @@ public class App {
 
         sc.close();
     }
+
+    private void modify(Rq rq) {
+        // URL에 입력된 id 얻기
+        int paramId = rq.getIntParam("id", 0);
+
+        // URL에 입력된 id가 없다면 작업중지
+        if (paramId == 0) {
+            System.out.println("id를 입력해주세요.");
+            return;
+        }
+
+        // URL에 입력된 id에 해당하는 명언객체 찾기
+        WiseSaying foundWiseSaying = findById(paramId);
+
+        // 찾지 못했다면 중지
+        if (foundWiseSaying == null) {
+            System.out.printf("%d번 명언은 존재하지 않습니다.\n", paramId);
+            return;
+        }
+
+        System.out.printf("명언(기존) : %s\n", foundWiseSaying.content);
+        System.out.printf("명언 : ");
+        foundWiseSaying.content = sc.nextLine();
+        System.out.printf("작가(기존) : %s\n", foundWiseSaying.author);
+        System.out.printf("작가 : ");
+        foundWiseSaying.author = sc.nextLine();
+
+        System.out.printf("%d번 명언이 수정되었습니다.\n", paramId);
+    }
+
     private void list(Rq rq) {
         System.out.println("번호 / 작가 / 명언");
         System.out.println("-------------------");
